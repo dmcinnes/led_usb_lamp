232c5abdc70a56acdea131e3156abeb8f47c74a8
Doug McInnes <doug@dougmcinnes.com>
2013-01-31 23:14:58 -0800

    paste in the RGB lib
    can't use ISR since we can't use the timer interrupts

--- led_usb_lamp.ino
+++ led_usb_lamp.ino
@@ -2,13 +2,31 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>     /* for _delay_ms() */
 #include <usbdrv.h>
-#include <DigisparkRGB.h>
+
+#include <avr/io.h>
+
+
+#define set(x) |= (1<<x)
+#define clr(x) &=~(1<<x)
+#define inv(x) ^=(1<<x)
+
+#define BLUE_CLEAR (pinlevelB &= ~(1 << BLUE)) // map BLUE to PB2
+#define GREEN_CLEAR (pinlevelB &= ~(1 << GREEN)) // map BLUE to PB2
+#define RED_CLEAR (pinlevelB &= ~(1 << RED)) // map BLUE to PB2
+#define PORTB_MASK  (1 << PB0) | (1 << PB1) | (1 << PB2)
+#define BLUE PB2
+#define GREEN PB1
+#define RED PB0
 
 
 #define STATE_CYCLE   0
 #define STATE_HOLD    1
 #define STATE_BEDTIME 2
 #define STATE_SLEEP   3
+
+
+unsigned char DigisparkPWMcompare[3];
+volatile unsigned char DigisparkPWMcompbuff[3];
 
 uint8_t color[]     = {255, 255, 255};
 uint8_t nextColor[] = {0, 0, 0};
@@ -29,7 +47,9 @@ unsigned char lastSof = -1;
 
 
 // the setup routine runs once when you press reset:
-void setup()  { 
+void setup()  {
+  DigisparkRGBBegin();
+
   // disable timer 0 overflow interrupt (used for millis)
   TIMSK&=!(1<<TOIE0);
 
@@ -49,12 +69,8 @@ void setup()  {
 
   usbPoll();
 
-  digitalWrite(0, HIGH);
-  digitalWrite(1, HIGH);
-  digitalWrite(2, HIGH);
-
-  DigisparkRGBBegin();
-} 
+  chooseNextColor();
+}
 
 
 void loop () {
@@ -87,6 +103,8 @@ void loop () {
       nextColor[1] = 0;
       nextColor[2] = 0;
     } else if (state == STATE_SLEEP) {
+      // wake up
+      chooseNextColor();
       state = STATE_CYCLE;
     }
 
@@ -96,6 +114,10 @@ void loop () {
   if (currentMillis - lastRefresh > 50) {
     lastRefresh = currentMillis;
     usbPoll();
+  }
+
+  if (state != STATE_SLEEP) {
+    DigisparkRGBUpdate();
   }
 }
 
@@ -139,7 +161,7 @@ void cycleToNextColor() {
 }
 
 void holdColor() {
-  if (currentMillis - holdMillis > 10000) { // 10 seconds
+  if (currentMillis - holdMillis > 2000) { // 2 seconds
     holdMillis = currentMillis;
     chooseNextColor();
     state = STATE_CYCLE;
@@ -147,7 +169,7 @@ void holdColor() {
 }
 
 void shuttingDown() {
-  if (currentMillis - holdMillis > 50) {
+  if (currentMillis - holdMillis > 30) {
     holdMillis = currentMillis;
 
     shiftColors();
@@ -156,4 +178,61 @@ void shuttingDown() {
       state = STATE_SLEEP;
     }
   }
+}
+
+// Digispark RGB code
+
+void DigisparkRGBBegin() {
+
+  pinMode(2, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(0, OUTPUT);
+  //CLKPR = (1 << CLKPCE);        // enable clock prescaler update
+  //CLKPR = 0;                    // set clock to maximum (= crystal)
+
+
+  DigisparkPWMcompare[0] = 0x00;// set default PWM values
+  DigisparkPWMcompare[1] = 0x00;// set default PWM values
+  DigisparkPWMcompare[2] = 0x00;// set default PWM values
+  DigisparkPWMcompbuff[0] = 0x00;// set default PWM values
+  DigisparkPWMcompbuff[1] = 0x00;// set default PWM values
+  DigisparkPWMcompbuff[2] = 0x00;// set default PWM values
+
+
+  TIFR = (1 << TOV0);           // clear interrupt flag
+  TIMSK = (1 << TOIE0);         // enable overflow interrupt
+  TCCR0B = (1 << CS00);         // start timer, no prescale
+
+  sei();
+}
+
+void DigisparkRGB(int pin,int value){
+	DigisparkPWMcompbuff[pin] = value;
+}
+
+void DigisparkRGBDelay(int ms) {
+  while (ms) {
+    _delay_ms(1);
+    ms--;
+  }
+}
+
+void DigisparkRGBUpdate() {
+  static unsigned char pinlevelB=PORTB_MASK;
+  static unsigned char softcount=0xFF;
+
+  PORTB = pinlevelB;            // update outputs
+
+  if(++softcount == 0){         // increment modulo 256 counter and update
+                                // the compare values only when counter = 0.
+    DigisparkPWMcompare[0] = DigisparkPWMcompbuff[0]; // verbose code for speed
+    DigisparkPWMcompare[1] = DigisparkPWMcompbuff[1]; // verbose code for speed
+    DigisparkPWMcompare[2] = DigisparkPWMcompbuff[2]; // verbose code for speed
+
+    pinlevelB = PORTB_MASK;     // set all port pins high
+  }
+  // clear port pin on compare match (executed on next interrupt)
+  if(DigisparkPWMcompare[0] == softcount) RED_CLEAR;
+  if(DigisparkPWMcompare[1] == softcount) GREEN_CLEAR;
+  if(DigisparkPWMcompare[2] == softcount) BLUE_CLEAR;
 }
