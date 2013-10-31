1de85ba09947611f0b3d6f1dac75fac77d8c8e06
Doug McInnes <doug@dougmcinnes.com>
2013-10-30 23:57:00 -0700

    re-introduce the old fader code
    but use the delay functions instead of counting milliseconds

--- led_usb_lamp.ino
+++ led_usb_lamp.ino
@@ -1,26 +1,25 @@
 #include <avr/interrupt.h>
 #include <DigisparkRGB.h>
-/*
-   Digispark RGB
 
-   This example shows how to use soft PWM to fade 3 colors.
-Note: This is only necessary for PB2 (pin 2) - Blue, as Red (pin 0) and Green (pin 1) as well as pin 4 support the standard Arduino analogWrite() function.
+#define STATE_CYCLE   0
+#define STATE_HOLD    1
+#define STATE_BEDTIME 2
+#define STATE_SLEEP   3
 
-This example code is in the public domain.
-*/
-byte RED = 0;
-byte BLUE = 2;
-byte GREEN = 1;
-byte COLORS[] = {RED, BLUE, GREEN};
+// in milliseconds
+// time between 1/255 color changes
+#define SHIFT_TIMEOUT      10
+// time it holds a chosen color
+#define HOLD_COLOR_TIMEOUT 2000
 
-boolean dir = true;
-int currentColor = 0;
+uint8_t state = STATE_CYCLE;
 
-unsigned long counter         = 0;
-unsigned long previousCounter = 1;
+unsigned char color[]     = {255, 255, 255};
+unsigned char nextColor[] = {0, 0, 0};
 
-bool off   = false;
-bool sleep = false;
+unsigned int counter         = 1;
+unsigned int previousCounter = 0;
+
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -33,59 +32,111 @@ void setup() {
 
   // Turn on global interrupts
   sei();
+
+  chooseNextColor();
 }
 
 void loop () {
-  if (off) {
-    // do nothing
-  } else if (sleep) {
-    fade(0, false);
-    fade(1, false);
-    fade(2, false);
-    off = true;
-    dir = true; // so it fades back up
-  } else {
-    fade(COLORS[currentColor], dir);
-    currentColor = (currentColor+1) % 3;
-    dir = !dir;
+  checkSleep();
+
+  switch (state) {
+    case STATE_CYCLE:
+      cycleToNextColor();
+      break;
+    case STATE_HOLD:
+      holdColor();
+      break;
+    case STATE_BEDTIME:
+      shuttingDown();
+      break;
   }
 }
 
-bool checkSleep() {
-  if (counter == previousCounter && !sleep) {
-    sleep = true;
-    return true;
+bool match() {
+  return color[0] == nextColor[0] &&
+         color[1] == nextColor[1] &&
+         color[2] == nextColor[2];
+}
+
+void shiftColors() {
+  unsigned int i;
+
+  DigisparkRGBDelay(SHIFT_TIMEOUT);
+
+  for (i = 0; i < 3; i++) {
+    if (color[i] < nextColor[i]) {
+      color[i]++;
+    } else if (color[i] > nextColor[i]) {
+      color[i]--;
+    }
   }
+
+  DigisparkRGB(0, color[0]);
+  DigisparkRGB(1, color[1]);
+  DigisparkRGB(2, color[2]);
+}
+
+void chooseNextColor() {
+  unsigned int i;
+
+  for (i = 0; i < 3; i++) {
+    nextColor[i] = random(255);
+    if (nextColor[i] < 50) {
+      nextColor[i] = 0;
+    }
+  }
+
+  if (nextColor[0] == 0 &&
+      nextColor[1] == 0 &&
+      nextColor[2] == 0) {
+    chooseNextColor(); // try again
+  }
+}
+
+// *** STATES ***
+
+void cycleToNextColor() {
+  if (match()) {
+    state = STATE_HOLD;
+  } else {
+    shiftColors();
+  }
+}
+
+void holdColor() {
+  DigisparkRGBDelay(HOLD_COLOR_TIMEOUT);
+  chooseNextColor();
+  state = STATE_CYCLE;
+}
+
+void shuttingDown() {
+  shiftColors();
+
+  if (match()) {
+    state = STATE_SLEEP;
+  }
+}
+
+void checkSleep() {
+  if (counter == previousCounter) {
+    // counter's not updating, go to sleep
+    state = STATE_BEDTIME;
+
+    // fade to zero
+    nextColor[0] = 0;
+    nextColor[1] = 0;
+    nextColor[2] = 0;
+
+  } else if (state == STATE_SLEEP) {
+    // We're asleep and counter is now updating, wake up
+    state = STATE_CYCLE;
+    chooseNextColor();
+  }
+
   previousCounter = counter;
-  return false;
-}
-
-void fade(byte Led, boolean dir) {
-  int i;
-
-  //if fading up
-  if (dir) {
-    for (i = 0; i < 256; i++) {
-      DigisparkRGB(Led, i);
-      DigisparkRGBDelay(25);
-      if (checkSleep()) {
-        break;
-      }
-    }
-  } else {
-    for (i = 255; i >= 0; i--) {
-      DigisparkRGB(Led, i);
-      DigisparkRGBDelay(25);
-      if (checkSleep()) {
-        break;
-      }
-    }
-  }
 }
 
 // called when data appears on the USB pins
 ISR(PCINT0_vect) {
   counter++;
-  off = false;
-  sleep = false;
 }
