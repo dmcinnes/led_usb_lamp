95f1803aa53d320653e593b5b37fddfe56b1617d
Doug McInnes <doug@dougmcinnes.com>
2013-10-30 23:18:08 -0700

    rewritten using DigisparkRGB and simple interrupts

--- led_usb_lamp.ino
+++ led_usb_lamp.ino
@@ -1,246 +1,91 @@
-#include <avr/sleep.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>     /* for _delay_ms() */
-#include <usbdrv.h>
+#include <DigisparkRGB.h>
+/*
+   Digispark RGB
 
-#include <avr/io.h>
+   This example shows how to use soft PWM to fade 3 colors.
+Note: This is only necessary for PB2 (pin 2) - Blue, as Red (pin 0) and Green (pin 1) as well as pin 4 support the standard Arduino analogWrite() function.
 
+This example code is in the public domain.
+*/
+byte RED = 0;
+byte BLUE = 2;
+byte GREEN = 1;
+byte COLORS[] = {RED, BLUE, GREEN};
 
-#define set(x) |= (1<<x)
-#define clr(x) &=~(1<<x)
-#define inv(x) ^=(1<<x)
+boolean dir = true;
+int currentColor = 0;
 
-#define BLUE_CLEAR (pinlevelB &= ~(1 << BLUE)) // map BLUE to PB2
-#define GREEN_CLEAR (pinlevelB &= ~(1 << GREEN)) // map BLUE to PB2
-#define RED_CLEAR (pinlevelB &= ~(1 << RED)) // map BLUE to PB2
-#define PORTB_MASK  (1 << PB0) | (1 << PB1) | (1 << PB2)
-#define BLUE PB2
-#define GREEN PB1
-#define RED PB0
+unsigned long counter         = 0;
+unsigned long previousCounter = 1;
 
-
-#define STATE_CYCLE   0
-#define STATE_HOLD    1
-#define STATE_BEDTIME 2
-#define STATE_SLEEP   3
-
-
-unsigned char DigisparkPWMcompare[3];
-volatile unsigned char DigisparkPWMcompbuff[3];
-
-unsigned char color[]     = {255, 255, 255};
-unsigned char nextColor[] = {0, 0, 0};
-uint8_t i, j;
-unsigned long currentMillis = 0;
-unsigned long lastMillis    = 0;
-unsigned long holdMillis    = 0;
-unsigned long lastRefresh   = 0;
-
-unsigned int counter = 0;
-
-uint8_t state = STATE_CYCLE;
-
-// update count from the USB host
-extern volatile unsigned char usbSofCount = 0;
-unsigned char lastSof = -1;
-
+bool off   = false;
+bool sleep = false;
 
 // the setup routine runs once when you press reset:
-void setup()  {
+void setup() {
   DigisparkRGBBegin();
 
-  // disable timer 0 overflow interrupt (used for millis)
-  TIMSK&=!(1<<TOIE0);
+  // Enable interrupts only on pins 3 and 4
+  PCMSK = (1<<PCINT3) | (1<<PCINT4);
+  // Enable PCINT interrupts
+  GIMSK = (1<<PCIE);
 
-  cli();
-
-  usbInit();
-
-  usbDeviceDisconnect();
-  uchar   i;
-  i = 0;
-  while(--i){             /* fake USB disconnect for > 250 ms */
-    _delay_ms(1);
-  }
-  usbDeviceConnect();
-
+  // Turn on global interrupts
   sei();
-
-  usbPoll();
-
-  chooseNextColor();
 }
-
 
 void loop () {
-  // fake the millisecond counter
-  counter++;
-  if (counter % 100 == 0) {
-    currentMillis++;
-  }
-
-  switch (state) {
-    case STATE_CYCLE:
-      cycleToNextColor();
-      break;
-    case STATE_HOLD:
-      holdColor();
-      break;
-    case STATE_BEDTIME:
-      shuttingDown();
-      break;
-  }
-
-  if (currentMillis - lastMillis > 1000) {
-    lastMillis = currentMillis;
-
-    if (lastSof == usbSofCount) {
-      state = STATE_BEDTIME;
-
-      // fade to zero
-      nextColor[0] = 0;
-      nextColor[1] = 0;
-      nextColor[2] = 0;
-    } else if (state == STATE_SLEEP) {
-      // wake up
-      chooseNextColor();
-      state = STATE_CYCLE;
-    }
-
-    lastSof = usbSofCount;
-  }
-
-  if (currentMillis - lastRefresh > 50) {
-    lastRefresh = currentMillis;
-    usbPoll();
-  }
-
-  if (state != STATE_SLEEP) {
-    DigisparkRGBUpdate();
-  }
-}
-
-bool match() {
-  return color[0] == nextColor[0] &&
-         color[1] == nextColor[1] &&
-         color[2] == nextColor[2];
-}
-
-void shiftColors() {
-  for (i = 0; i < 3; i++) {
-    if (color[i] < nextColor[i]) {
-      color[i]++;
-    } else if (color[i] > nextColor[i]) {
-      color[i]--;
-    }
-  }
-
-  DigisparkRGB(0, color[0]);
-  DigisparkRGB(1, color[1]);
-  DigisparkRGB(2, color[2]);
-}
-
-void chooseNextColor() {
-  for (i = 0; i < 3; i++) {
-    nextColor[i] = random(255);
-    if (nextColor[i] < 50) {
-      nextColor[i] = 0;
-    }
-  }
-
-  if (nextColor[0] == 0 &&
-      nextColor[1] == 0 &&
-      nextColor[2] == 0) {
-    chooseNextColor(); // try again
-  }
-}
-
-// *** STATES ***
-
-void cycleToNextColor() {
-  if (match()) {
-    state = STATE_HOLD;
+  if (off) {
+    // do nothing
+  } else if (sleep) {
+    fade(0, false);
+    fade(1, false);
+    fade(2, false);
+    off = true;
+    dir = true; // so it fades back up
   } else {
-    if (currentMillis - holdMillis > 10) {
-      holdMillis = currentMillis;
-      shiftColors();
+    fade(COLORS[currentColor], dir);
+    currentColor = (currentColor+1) % 3;
+    dir = !dir;
+  }
+}
+
+bool checkSleep() {
+  if (counter == previousCounter && !sleep) {
+    sleep = true;
+    return true;
+  }
+  previousCounter = counter;
+  return false;
+}
+
+void fade(byte Led, boolean dir) {
+  int i;
+
+  //if fading up
+  if (dir) {
+    for (i = 0; i < 256; i++) {
+      DigisparkRGB(Led, i);
+      DigisparkRGBDelay(25);
+      if (checkSleep()) {
+        break;
+      }
+    }
+  } else {
+    for (i = 255; i >= 0; i--) {
+      DigisparkRGB(Led, i);
+      DigisparkRGBDelay(25);
+      if (checkSleep()) {
+        break;
+      }
     }
   }
 }
 
-void holdColor() {
-  if (currentMillis - holdMillis > 2000) { // 2 seconds
-    holdMillis = currentMillis;
-    chooseNextColor();
-    state = STATE_CYCLE;
-  }
-}
-
-void shuttingDown() {
-  if (currentMillis - holdMillis > 30) {
-    holdMillis = currentMillis;
-
-    shiftColors();
-
-    if (match()) {
-      state = STATE_SLEEP;
-    }
-  }
-}
-
-// Digispark RGB code
-
-void DigisparkRGBBegin() {
-
-  pinMode(2, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(0, OUTPUT);
-  //CLKPR = (1 << CLKPCE);        // enable clock prescaler update
-  //CLKPR = 0;                    // set clock to maximum (= crystal)
-
-
-  DigisparkPWMcompare[0] = 0x00;// set default PWM values
-  DigisparkPWMcompare[1] = 0x00;// set default PWM values
-  DigisparkPWMcompare[2] = 0x00;// set default PWM values
-  DigisparkPWMcompbuff[0] = 0x00;// set default PWM values
-  DigisparkPWMcompbuff[1] = 0x00;// set default PWM values
-  DigisparkPWMcompbuff[2] = 0x00;// set default PWM values
-
-
-  TIFR = (1 << TOV0);           // clear interrupt flag
-  TIMSK = (1 << TOIE0);         // enable overflow interrupt
-  TCCR0B = (1 << CS00);         // start timer, no prescale
-
-  sei();
-}
-
-void DigisparkRGB(int pin, int value) {
-  DigisparkPWMcompbuff[pin] = value;
-}
-
-void DigisparkRGBDelay(int ms) {
-  while (ms) {
-    _delay_ms(1);
-    ms--;
-  }
-}
-
-void DigisparkRGBUpdate() {
-  static unsigned char pinlevelB=PORTB_MASK;
-  static unsigned char softcount=0xFF;
-
-  PORTB = pinlevelB;            // update outputs
-
-  if (++softcount == 0) {       // increment modulo 256 counter and update
-                                // the compare values only when counter = 0.
-    DigisparkPWMcompare[0] = DigisparkPWMcompbuff[0]; // verbose code for speed
-    DigisparkPWMcompare[1] = DigisparkPWMcompbuff[1]; // verbose code for speed
-    DigisparkPWMcompare[2] = DigisparkPWMcompbuff[2]; // verbose code for speed
-
-    pinlevelB = PORTB_MASK;     // set all port pins high
-  }
-  // clear port pin on compare match (executed on next interrupt)
-  if (DigisparkPWMcompare[0] == softcount) RED_CLEAR;
-  if (DigisparkPWMcompare[1] == softcount) GREEN_CLEAR;
-  if (DigisparkPWMcompare[2] == softcount) BLUE_CLEAR;
+// called when data appears on the USB pins
+ISR(PCINT0_vect) {
+  counter++;
+  off = false;
+  sleep = false;
 }
