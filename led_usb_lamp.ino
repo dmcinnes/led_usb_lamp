a4082a8e68c45517a984e937fb3dad6aae85bfb4
Doug McInnes <doug@dougmcinnes.com>
2014-03-30 11:10:52 -0700

    Only sleep/wake when receiving interrupts
    when micronucleus runs it sets up the USB configuration. So on first
    plugin, the computer recognizes it as a USB device and continuely sends
    heartbeat messages. Every other time, the digispark is not a USB device
    so the computer won't send heartbeats. It does, however, receive a few
    events when the hub is first plugged in and after it is unplugged so we
    switch between wake and sleep events whenever we see activity on the USB
    data pins. Also added a 10 second timeout so it can reach a steady
    state.
    To get this to work properly, plug the digispark into the USB hub--while
    the hub is disconnected from the computeer--and wait 10 seconds. The lamp
    should not light up. Then you can plug the hub into your computer and
    the lamp will turn on.

--- led_usb_lamp.ino
+++ led_usb_lamp.ino
@@ -11,21 +11,19 @@
 #define SHIFT_TIMEOUT      25
 // time it holds a chosen color
 #define HOLD_COLOR_TIMEOUT 2000
+// how long to wait for the pin changes to stop
+#define WAIT_TIMEOUT       10000
 
-uint8_t state = STATE_CYCLE;
+uint8_t state = STATE_SLEEP;
 
-unsigned char color[]     = {255, 255, 255};
+unsigned char color[]     = {0, 0, 0};
 unsigned char nextColor[] = {0, 0, 0};
 
-unsigned int counter;
-unsigned int previousCounter;
+unsigned int counter = 0;
 
 
 // the setup routine runs once when you press reset:
 void setup() {
-  counter         = 1;
-  previousCounter = 0;
-
   DigisparkRGBBegin();
 
   // Enable interrupts only on pins 3 and 4
@@ -112,20 +110,20 @@ void holdColor() {
   chooseNextColor();
   state = STATE_CYCLE;
 
-  checkSleep();
+  checkForBedtime();
 }
 
 void shuttingDown() {
   shiftColors();
 
   if (match()) {
-    state = STATE_SLEEP;
+    goToSleep();
   }
 }
 
-void checkSleep() {
-  if (counter == previousCounter) {
-    // counter's not updating, go to sleep
+void checkForBedtime() {
+  if (counter != 0) {
+    // counter flipped
     state = STATE_BEDTIME;
 
     // fade to zero
@@ -133,31 +131,28 @@ void checkSleep() {
     nextColor[1] = 0;
     nextColor[2] = 0;
   }
-
-  previousCounter = counter;
 }
 
 void checkForWake() {
-  if (counter != previousCounter) {
-    // We're asleep and counter is now updating, wake up
+  if (counter != 0) {
+    // We're asleep and counter flipped, wake up
     chooseNextColor();
     state = STATE_CYCLE;
 
-    // wait 5 seconds for things to mellow out
-    DigisparkRGBDelay(5000);
-
-    // --- Reboot the Digispark so it can get in a good state again
-    reboot();
+    clearCounter();
   }
 }
 
-void reboot(void) {
-  noInterrupts(); // disable interrupts which could mess with changing prescaler
-  CLKPR = 0b10000000; // enable prescaler speed change
-  CLKPR = 0; // set prescaler to default (16mhz) mode required by bootloader
-  void (*ptrToFunction)(); // allocate a function pointer
-  ptrToFunction = 0x0000; // set function pointer to bootloader reset vector
-  (*ptrToFunction)(); // jump to reset, which bounces in to bootloader
+void goToSleep() {
+  state = STATE_SLEEP;
+  clearCounter();
+}
+
+void clearCounter() {
+  // wait for things to mellow out
+  DigisparkRGBDelay(WAIT_TIMEOUT);
+
+  counter = 0;
 }
 
 // called when data appears on the USB pins
